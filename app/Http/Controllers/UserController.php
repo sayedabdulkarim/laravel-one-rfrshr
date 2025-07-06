4f2d193d27e81dff600b313370432e0ff47b257e
Sayed Abdul <sakarim9124@gmail.com>
2025-07-06 18:44:17 +0530

    Refactor register method to handle user input and validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,17 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
-    function register(){
-        return 'Hello from our comtroller.';
+    function register(Request $request){
+        echo $request->input('name');
+        echo $request->input('email');
+        echo $request->input('password');
+
+        $incomingFields = $request->validate([
+            'name' => 'required|min:3|max:20',
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:20'
+        ]);
+
+        return 'Thank you for registering';
     }
 }
